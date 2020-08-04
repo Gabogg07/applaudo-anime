2675761b707cfcef25a08fe99196a5b2390287c7
Gabriel Gutierrez <gabrieleduardogg@gmail.com>
2020-08-03 22:09:31 -0700

    Initial project with navigation

---
+++ src/Components/HorizontalList/HorizontalList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class HorizontalList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <Flatlist
+
+      />
+    );
+  }
+}
+
+export default HorizontalList;
