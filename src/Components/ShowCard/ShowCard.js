907e878ccf8b824a25be2357831cf9af66721112
Gabriel Gutierrez <gabrieleduardogg@gmail.com>
2020-08-04 09:50:46 -0700

    Navigation from card to show detail

--- src/Components/ShowCard/ShowCard.js
+++ src/Components/ShowCard/ShowCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image} from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableWithoutFeedback} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 class ShowCard extends Component {
     constructor(props) {
@@ -9,64 +10,40 @@ class ShowCard extends Component {
     }
 
     render() {  
-        const { props } = this
+        const {props} = this;
+        const { show, navigation } = props;
         return (
-            <View style={{margin:5, borderColor:'black',
-            borderWidth:2,}}>
-                <Image 
-                    style={[styles.cardContainer, this.props.styles]}
-                    source={{uri:this.props.show.attributes.posterImage.medium}}
-                />
-                <View style={[this.props.styles, {backgroundColor:'gray', flex:1, justifyContent:'center', alignItems:'center'}]}>
-                    <Text style={{ellipsizeMode:'tail'}}>{this.props.show.attributes.titles.en || this.props.show.attributes.titles.en_jp}</Text>
+            <TouchableWithoutFeedback
+                onPress={()=>navigation.navigate('ShowDetail')}
+            >
+
+                <View style={{margin:5, borderColor:'black',
+                borderWidth:2,}}>
+                    <Image 
+                        style={[styles.cardContainer, props.styles]}
+                        source={{uri:show.attributes.posterImage.medium}}
+                    />
+                    <View style={[props.styles, {backgroundColor:'gray', flex:1, justifyContent:'center', alignItems:'center'}]}>
+                        <Text style={{ellipsizeMode:'tail'}}>{show.attributes.titles.en || show.attributes.titles.en_jp}</Text>
+                    </View>
                 </View>
-            </View>
+            </TouchableWithoutFeedback>
+
 
        
         );
   }
 }
 
-export default ShowCard;
+export default function(props) {
+    const navigation = useNavigation();
+  
+    return <ShowCard {...props} navigation={navigation} />;
+  }
 
 const styles = StyleSheet.create({
     cardContainer:{
-
         aspectRatio:3/4
     }
 })
 
-// import React, { Component } from 'react';
-// import { View, Text, StyleSheet, Image} from 'react-native';
-
-// class ShowCard extends Component {
-//     constructor(props) {
-//         super(props);
-//             this.state = {
-//         };
-//     }
-
-//     render() {  
-//         const { props } = this
-//         return (
-//             <Image 
-//                 style={[styles.cardContainer, this.props.styles]}
-//                 source={uri:this.props.show.attributes.posterImage.medium}
-//             >
-//                 <Text> ShowCard </Text>
-//             </View>
-//         );
-//   }
-// }
-
-// export default ShowCard;
-
-// const styles = StyleSheet.create({
-//     cardContainer:{
-//         flex:1,
-//         borderColor:'black',
-//         borderWidth:2,
-//         margin:5,
-//         aspectRatio:3/4
-//     }
-// })
